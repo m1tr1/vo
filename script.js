dd23f1b92f1622861134906e2ead43fb57e853a8
m1tr1 <miritikris@gmail.com>
2023-09-04 15:23:34 +0300

    five

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const navs = document.querySelectorAll('.list')
 const blurs = document.querySelectorAll('.blur')
 const tops = document.querySelectorAll('.top')
 
-navs.forEach((nav) => {
-    nav.addEventListener('click', () => {
-        removeActiveNav()
-        nav.classList.toggle('active')
-    })
-})
+// navs.forEach((nav) => {
+//     nav.addEventListener('click', () => {
+//         removeActiveNav()
+//         nav.classList.toggle('active')
+//     })
+// })
 
-function removeActiveNav() {
-    navs.forEach((nav) => {
-        nav.classList.remove('active')
-    })
-    console.log(21)
-}
+// function removeActiveNav() {
+//     navs.forEach((nav) => {
+//         nav.classList.remove('active')
+//     })
+//     console.log(21)
+// }
 
 // blurs.forEach((blur) => {
 //     blur.addEventListener('mouseover', () => {
